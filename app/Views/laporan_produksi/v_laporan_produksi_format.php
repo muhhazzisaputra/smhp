09cf722ea4466f3b0a5ecdba47c1ec9a779a6c19
muhhazzisaputra <hazzihazzihazzi@gmail.com>
2025-06-03 21:34:05 +0700

    update report

--- app/Views/laporan_produksi/v_laporan_produksi_format.php
+++ app/Views/laporan_produksi/v_laporan_produksi_format.php
@@ -5,8 +5,8 @@
 			<td style="width: 108px;">&nbsp;&nbsp;&nbsp;Tgl Produksi</td>
 			<td style="width: 13px;">:</td>
 			<td style="">
-				<input type="text" name="tgl_src" id="tgl_src" value="<?php echo date('Y-m-01') ?>" class="form-control dateclass" style="width: 100px; background-color: white; display: inline;" readonly> -
-				<input type="text" name="tgl2_src" id="tgl2_src" value="<?php echo date('Y-m-d') ?>" class="form-control dateclass" style="width: 100px; background-color: white; display: inline;" readonly>
+				<input type="text" name="tgl_src" id="tgl_src" value="<?php echo date('Y-m-01') ?>" class="form-control dateclass" style="width: 107px; background-color: white; display: inline;" readonly> -
+				<input type="text" name="tgl2_src" id="tgl2_src" value="<?php echo date('Y-m-d') ?>" class="form-control dateclass" style="width: 107px; background-color: white; display: inline;" readonly>
 			</td>
 			<td style="width: 75px;">&nbsp;&nbsp;&nbsp;Produk</td>
 			<td style="width: 13px;">:</td>
@@ -31,7 +31,7 @@
 			<td style="width: 65px;">&nbsp;&nbsp;&nbsp;Bulan</td>
 			<td style="width: 13px;">:</td>
 			<td style="">
-				<input type="text" name="tgl_src" id="tgl_src" value="<?php echo date('Y-m-01') ?>" class="form-control monthclass" style="width: 100px; background-color: white; display: inline;" readonly>
+				<input type="text" name="tgl_src" id="tgl_src" value="<?php echo date('Y-m-01') ?>" class="form-control monthclass" style="width: 107px; background-color: white; display: inline;" readonly>
 			</td>
 			<?php if($format=="per_shift") { ?> 
 			<td style="width: 55px;">&nbsp;&nbsp;&nbsp;Shift</td>
